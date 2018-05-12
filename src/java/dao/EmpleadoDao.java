2899c875b2af3dfd05a8a80efe676cef60fb2789
juan valerio <>
2018-05-12 00:14:09 -0500

    Modifique UbigOrig lo que falto

---
+++ src/java/dao/EmpleadoDao.java
@@ -0,0 +1,179 @@
+package dao;
+
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+import modelo.Empleado;
+
+public class EmpleadoDao extends DAO {
+
+    public void registrar(Empleado emp) throws Exception {
+        try {
+            this.Conexion();
+            PreparedStatement st = this.getCn().prepareStatement("INSERT INTO Empleado (DNI,Nom,ApelPate,ApelMate,RUC,Email,Telf,Cel,FecNac,GrupSang,EstCiv,ConLab,CarnAseg,Refe,Leye,FecIng,FecNom,DatCony,UbigActu,UbigOrig,Est) values(?,?,?,?, ?,?,?,?,?, ?,?,?,?,?, ?,?,?,?,?,?,?)");
+            st.setString(1, emp.getDNI());
+            st.setString(2, emp.getNom());
+            st.setString(3, emp.getApelPate());
+            st.setString(4, emp.getApelMate());
+            st.setString(5, emp.getRUC());
+            st.setString(6, emp.getEmail());
+            st.setString(7, emp.getTelf());
+            st.setString(8, emp.getCel());
+            st.setString(9, emp.getFecNac());
+            st.setString(10, emp.getGrupSang());
+            st.setString(11, emp.getEstCiv());
+            st.setString(12, emp.getConLab());
+            st.setString(13, emp.getCarnAseg());
+            st.setString(14, emp.getRefe());
+            st.setString(15, emp.getLeye());
+            st.setString(16, emp.getFecIng());
+            st.setString(17, emp.getFecNom());
+            st.setString(18, emp.getDatCony());
+            st.setString(19, emp.getUbigActu());
+            st.setString(20, emp.getUbigOrig());
+            st.setString(21, emp.getEst());
+
+            st.executeUpdate();
+        } catch (SQLException e) {
+            throw e;
+        } finally {
+            this.Cerrar();
+        }
+
+    }
+
+    public List<Empleado> listar() throws Exception {
+        List<Empleado> lista;
+        ResultSet rs;
+
+        try {
+            this.Conexion();
+            PreparedStatement st = this.getCn().prepareCall("SELECT idEmpl, DNI,Nom,ApelPate,ApelMate,RUC,Email,Telf,Cel,FecNac,GrupSang,EstCiv,ConLab,CarnAseg,Refe,Leye,FecIng,FecNom,DatCony,UbigActu,UbigOrig,Est FROM Empleado");
+            rs = st.executeQuery();
+            lista = new ArrayList();
+            while (rs.next()) {
+                Empleado emp = new Empleado();
+                emp.setIdEmpl(rs.getInt("idEmpl"));
+                emp.setDNI(rs.getString("DNI"));
+                emp.setNom(rs.getString("Nom"));
+                emp.setApelPate(rs.getString("ApelPate"));
+                emp.setApelMate(rs.getString("ApelMate"));
+                emp.setRUC(rs.getString("RUC"));
+                emp.setEmail(rs.getString("Email"));
+                emp.setTelf(rs.getString("Telf"));
+                emp.setCel(rs.getString("Cel"));
+                emp.setFecNac(rs.getString("FecNac"));
+                emp.setGrupSang(rs.getString("GrupSang"));
+                emp.setEstCiv(rs.getString("EstCiv"));
+                emp.setConLab(rs.getString("ConLab"));
+                emp.setCarnAseg(rs.getString("CarnAseg"));
+                emp.setRefe(rs.getString("Refe"));
+                emp.setLeye(rs.getString("Leye"));
+                emp.setFecIng(rs.getString("FecIng"));
+                emp.setFecNom(rs.getString("FecNom"));
+                emp.setDatCony(rs.getString("DatCony"));
+                emp.setUbigActu(rs.getString("UbigActu"));
+                emp.setUbigOrig(rs.getString("UbigOrig"));
+                emp.setEst(rs.getString("Est"));
+
+                lista.add(emp);
+            }
+        } catch (SQLException e) {
+            throw e;
+        } finally {
+            this.Cerrar();
+        }
+        return lista;
+    }
+
+    public Empleado leerID(Empleado emp) throws Exception {
+        Empleado emple = null;
+        ResultSet rs;
+
+        try {
+            this.Conexion();
+            PreparedStatement st = this.getCn().prepareStatement("SELECT idEmpl, DNI,Nom,ApelPate,ApelMate,RUC,Email,Telf,Cel,FecNac,GrupSang,EstCiv,ConLab,CarnAseg,Refe,Leye,FecIng,FecNom,DatCony,UbigActu,UbigOrig,Est FROM Empleado WHERE idEmpl=?");
+            st.setInt(1, emp.getIdEmpl());
+            rs = st.executeQuery();
+            while (rs.next()) {
+                emple = new Empleado();
+                emple.setIdEmpl(rs.getInt("idEmpl"));
+                emple.setDNI(rs.getString("DNI"));
+                emple.setNom(rs.getString("Nom"));
+                emple.setApelPate(rs.getString("ApelPate"));
+                emple.setApelMate(rs.getString("ApelMate"));
+                emple.setRUC(rs.getString("RUC"));
+                emple.setEmail(rs.getString("Email"));
+                emple.setTelf(rs.getString("Telf"));
+                emple.setCel(rs.getString("Cel"));
+                emple.setFecNac(rs.getString("FecNac"));
+                emple.setGrupSang(rs.getString("GrupSang"));
+                emple.setEstCiv(rs.getString("EstCiv"));
+                emple.setConLab(rs.getString("ConLab"));
+                emple.setCarnAseg(rs.getString("CarnAseg"));
+                emple.setRefe(rs.getString("Refe"));
+                emple.setLeye(rs.getString("Leye"));
+                emple.setFecIng(rs.getString("FecIng"));
+                emple.setFecNom(rs.getString("FecNom"));
+                emple.setDatCony(rs.getString("DatCony"));
+                emple.setUbigActu(rs.getString("UbigActu"));
+                emple.setUbigOrig(rs.getString("UbigOrig"));
+                emple.setEst(rs.getString("Est"));
+            }
+        } catch (SQLException e) {
+            throw e;
+        } finally {
+            this.Cerrar();
+        }
+        return emple;
+    }
+
+    public void modificar(Empleado emp) throws Exception {
+        try {
+            this.Conexion();
+            PreparedStatement st = this.getCn().prepareStatement("UPDATE Empleado SET DNI = ?, Nom = ?, ApelPate = ?, ApelMate = ?,RUC = ?, Email = ?,Telf = ?,Cel = ?,FecNac = ?,GrupSang = ?,EstCiv = ?,ConLab = ?,CarnAseg = ?,Refe = ?,Leye = ?,FecIng = ?,FecNom = ?,DatCony = ?,UbigActu = ?,UbigOrig = ?,Est = ? WHERE idEmpl = ?");
+            st.setString(1, emp.getDNI());
+            st.setString(2, emp.getNom());
+            st.setString(3, emp.getApelPate());
+            st.setString(4, emp.getApelMate());
+            st.setString(5, emp.getRUC());
+            st.setString(6, emp.getEmail());
+            st.setString(7, emp.getTelf());
+            st.setString(8, emp.getCel());
+            st.setString(9, emp.getFecNac());
+            st.setString(10, emp.getGrupSang());
+            st.setString(11, emp.getEstCiv());
+            st.setString(12, emp.getConLab());
+            st.setString(13, emp.getCarnAseg());
+            st.setString(14, emp.getRefe());
+            st.setString(15, emp.getLeye());
+            st.setString(16, emp.getFecIng());
+            st.setString(17, emp.getFecNom());
+            st.setString(18, emp.getDatCony());
+            st.setString(19, emp.getUbigActu());
+            st.setString(20, emp.getUbigOrig());
+            st.setString(21, emp.getEst());
+            st.setInt(22, emp.getIdEmpl());
+            st.executeUpdate();
+        } catch (SQLException e) {
+            throw e;
+        } finally {
+            this.Cerrar();
+        }
+    }
+
+    public void eliminar(Empleado emp) throws Exception {
+        try {
+            this.Conexion();
+            PreparedStatement st = this.getCn().prepareStatement("DELETE FROM Empleado WHERE idEmpl = ?");
+            st.setInt(1, emp.getIdEmpl());
+            st.executeUpdate();
+        } catch (SQLException e) {
+            throw e;
+        } finally {
+            this.Cerrar();
+        }
+    }
+}
---
+++ src/java/modelo/Empleado.java
@@ -0,0 +1,186 @@
+package modelo;
+
+
+
+
+public class Empleado {
+    private int idEmpl;
+    private String DNI,Nom,ApelPate,ApelMate,RUC,Email,Telf,Cel,FecNac,GrupSang,EstCiv,ConLab,CarnAseg,Refe,Leye,FecIng,FecNom,DatCony,UbigActu,UbigOrig,Est;
+
+    public String getUbigOrig() {
+        return UbigOrig;
+    }
+
+    public void setUbigOrig(String UbigOrig) {
+        this.UbigOrig = UbigOrig;
+    }
+
+    public int getIdEmpl() {
+        return idEmpl;
+    }
+
+    public void setIdEmpl(int idEmpl) {
+        this.idEmpl = idEmpl;
+    }
+
+    public String getDNI() {
+        return DNI;
+    }
+
+    public void setDNI(String DNI) {
+        this.DNI = DNI;
+    }
+
+    public String getNom() {
+        return Nom;
+    }
+
+    public void setNom(String Nom) {
+        this.Nom = Nom;
+    }
+
+    public String getApelPate() {
+        return ApelPate;
+    }
+
+    public void setApelPate(String ApelPate) {
+        this.ApelPate = ApelPate;
+    }
+
+    public String getApelMate() {
+        return ApelMate;
+    }
+
+    public void setApelMate(String ApelMate) {
+        this.ApelMate = ApelMate;
+    }
+
+    public String getRUC() {
+        return RUC;
+    }
+
+    public void setRUC(String RUC) {
+        this.RUC = RUC;
+    }
+
+    public String getEmail() {
+        return Email;
+    }
+
+    public void setEmail(String Email) {
+        this.Email = Email;
+    }
+
+    public String getTelf() {
+        return Telf;
+    }
+
+    public void setTelf(String Telf) {
+        this.Telf = Telf;
+    }
+
+    public String getCel() {
+        return Cel;
+    }
+
+    public void setCel(String Cel) {
+        this.Cel = Cel;
+    }
+
+    public String getFecNac() {
+        return FecNac;
+    }
+
+    public void setFecNac(String FecNac) {
+        this.FecNac = FecNac;
+    }
+
+    public String getGrupSang() {
+        return GrupSang;
+    }
+
+    public void setGrupSang(String GrupSang) {
+        this.GrupSang = GrupSang;
+    }
+
+    public String getEstCiv() {
+        return EstCiv;
+    }
+
+    public void setEstCiv(String EstCiv) {
+        this.EstCiv = EstCiv;
+    }
+
+    public String getConLab() {
+        return ConLab;
+    }
+
+    public void setConLab(String ConLab) {
+        this.ConLab = ConLab;
+    }
+
+    public String getCarnAseg() {
+        return CarnAseg;
+    }
+
+    public void setCarnAseg(String CarnAseg) {
+        this.CarnAseg = CarnAseg;
+    }
+
+    public String getRefe() {
+        return Refe;
+    }
+
+    public void setRefe(String Refe) {
+        this.Refe = Refe;
+    }
+
+    public String getLeye() {
+        return Leye;
+    }
+
+    public void setLeye(String Leye) {
+        this.Leye = Leye;
+    }
+
+    public String getFecIng() {
+        return FecIng;
+    }
+
+    public void setFecIng(String FecIng) {
+        this.FecIng = FecIng;
+    }
+
+    public String getFecNom() {
+        return FecNom;
+    }
+
+    public void setFecNom(String FecNom) {
+        this.FecNom = FecNom;
+    }
+
+    public String getDatCony() {
+        return DatCony;
+    }
+
+    public void setDatCony(String DatCony) {
+        this.DatCony = DatCony;
+    }
+
+    public String getUbigActu() {
+        return UbigActu;
+    }
+
+    public void setUbigActu(String UbigActu) {
+        this.UbigActu = UbigActu;
+    }
+
+    public String getEst() {
+        return Est;
+    }
+
+    public void setEst(String Est) {
+        this.Est = Est;
+    }
+    
+}
