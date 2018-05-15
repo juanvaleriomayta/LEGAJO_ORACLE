7548b06e954e5e3b4555ceeb394390479ffb038f
Jose Luis Condori Jara <jcondori@vallegrande.edu.pe>
2018-05-15 15:20:56 -0500

    Solucion

--- src/java/bean/ContactosC.java
+++ src/java/bean/ContactosC.java
@@ -1,32 +1,115 @@
 package bean;
 
 import dao.ContactosDao;
+import java.io.Serializable;
 import java.util.List;
 import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
 import javax.faces.context.FacesContext;
-import javax.faces.view.ViewScoped;
 import modelo.Contactos;
-
-
 
 @ManagedBean
 @ViewScoped
-public class ContactosC {
+public class ContactosC implements Serializable{
 
     private Contactos contactos = new Contactos();
     private List<Contactos> lstContactos;
     private String accion;
 
-    public String getAccion() {
-        return accion;
+    public void operar() throws Exception {
+        switch (accion) {
+            case "Registrar":
+                this.registrar();
+                break;
+            case "Modificar":
+                this.modificar();
+                break;
+        }
     }
 
-    public void setAccion(String accion) {
-        this.limpiar();
-        this.accion = accion;
+    public void limpiar() {
+        this.contactos.setCodConEmp(0);
+        this.contactos.setTipDeFami("");
+        this.contactos.setNom("");
+        this.contactos.setApe("");
+        this.contactos.setPar("");
+        this.contactos.setOcu("");
+        this.contactos.setFecNac("");
+        this.contactos.setTel("");
+        this.contactos.setCel("");
+        this.contactos.setEstCiv("");
+        this.contactos.setVive("");
     }
 
+    private void registrar() throws Exception {
+        ContactosDao dao;
+        try {
+            dao = new ContactosDao();
+            dao.registrar(contactos);
+            this.listar();
+            FacesContext.getCurrentInstance().addMessage(null, new FacesMessage("Agregado con Exito"));
+        } catch (Exception e) {
+            throw e;
+        }
+    }
+
+    public void listar() throws Exception {
+        ContactosDao dao;
+        try {
+            dao = new ContactosDao();
+            lstContactos = dao.listar();
+            FacesContext.getCurrentInstance().addMessage(null, new FacesMessage("Listado con Exito"));
+        } catch (Exception e) {
+            throw e;
+        }
+    }
+
+    public void leerID(Contactos cont) throws Exception {
+        ContactosDao dao;
+        Contactos temp;
+        try {
+            dao = new ContactosDao();
+            temp = dao.leerID(cont);
+
+            if (temp != null) {
+                this.contactos = temp;
+                this.accion = "Modificar";
+            }
+        } catch (Exception e) {
+            throw e;
+        }
+    }
+
+    private void modificar() throws Exception {
+        ContactosDao dao;
+        try {
+            dao = new ContactosDao();
+            dao.modificar(contactos);
+            this.listar();
+            FacesContext.getCurrentInstance().addMessage(null, new FacesMessage("Modificado con Exito"));
+        } catch (Exception e) {
+            throw e;
+        }
+    }
+
+    public void eliminar(Contactos cont) throws Exception {
+        ContactosDao dao;
+        try {
+            dao = new ContactosDao();
+            dao.eliminar(cont);
+            this.listar();
+            FacesContext.getCurrentInstance().addMessage(null, new FacesMessage("Eliminado con Exito"));
+        } catch (Exception e) {
+            throw e;
+        }
+    }
+
+    /**
+     * Getter and Setter
+     *
+     * @return
+     */
     public Contactos getContactos() {
         return contactos;
     }
@@ -43,98 +126,12 @@ public class ContactosC {
         this.lstContactos = lstContactos;
     }
 
-    
-
-    public void operar() throws Exception {
-        switch (accion) {
-            case "Registrar":
-                this.registrar();
-                break;
-            case "Modificar":
-                this.modificar();
-                break;
-        }
-    }
-    
-    public void limpiar(){
-        this.contactos.setCodConEmp(0);
-        this.contactos.setTipDeFami("");
-        this.contactos.setNom("");
-        this.contactos.setApe("");
-        this.contactos.setPar("");
-        this.contactos.setOcu("");
-        this.contactos.setFecNac("");
-        this.contactos.setTel("");
-        this.contactos.setCel("");
-        this.contactos.setEstCiv("");
-        this.contactos.setVive("");
+    public String getAccion() {
+        return accion;
     }
 
-    private void registrar() throws Exception {
-        ContactosDao dao;
-
-        try {
-            dao = new ContactosDao();
-            dao.registrar(contactos);
-            this.listar();          
-            FacesContext.getCurrentInstance().addMessage(null, new FacesMessage("Agregado con Exito"));
-        } catch (Exception e) {
-            throw e;
-        }
+    public void setAccion(String accion) {
+        this.accion = accion;
     }
 
-    public void listar() throws Exception {
-        ContactosDao dao;
-
-        try {
-            dao = new ContactosDao();
-            lstContactos = dao.listar();
-            FacesContext.getCurrentInstance().addMessage(null, new FacesMessage("Listado con Exito"));
-        } catch (Exception e) {
-            throw e;
-        }
-    }
-
-    public void leerID(Contactos cont) throws Exception {
-        ContactosDao dao;
-        Contactos temp;
-
-        try {
-            dao = new ContactosDao();
-            temp = dao.leerID(cont);
-
-            if (temp != null) {
-                this.contactos = temp;
-                this.accion = "Modificar";
-            }
-        } catch (Exception e) {
-            throw e;
-        }
-    }
-
-    private void modificar() throws Exception {
-        ContactosDao dao;
-
-        try {
-            dao = new ContactosDao();
-            dao.modificar(contactos);
-            this.listar();
-            FacesContext.getCurrentInstance().addMessage(null, new FacesMessage("Modificado con Exito"));
-        } catch (Exception e) {
-            throw e;
-        }
-    }
-
-    public void eliminar(Contactos cont) throws Exception {
-        ContactosDao dao;
-
-        try {
-            dao = new ContactosDao();
-            dao.eliminar(cont);
-            this.listar();
-            FacesContext.getCurrentInstance().addMessage(null, new FacesMessage("Eliminado con Exito"));
-        } catch (Exception e) {
-            throw e;
-        }
-    }
 }
